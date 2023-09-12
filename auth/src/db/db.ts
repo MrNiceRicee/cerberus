e760fe10854dd2cf0df0cc712cd2f0dc4e5b37c4
Joshua Santos <JoshCSan@gmail.com>
2023-09-12 16:22:42 -0700

    - chore: update db

--- auth/src/db/db.ts
+++ auth/src/db/db.ts
@@ -1,13 +1,9 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 
-console.log(Bun.env);
 // create the connection
 const connection = connect({
-  host: Bun.env.DATABASE_HOST,
-  username: Bun.env.DATABASE_USERNAME,
-  password: Bun.env.DATABASE_PASSWORD,
-  // url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(connection);
