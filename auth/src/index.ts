8b9b1fc8b5693e2f5e227142e7fb3d4dae9b1278
Joshua Santos <JoshCSan@gmail.com>
2023-09-16 19:48:59 -0700

    - update: Elysia server init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,9 +3,7 @@ import { Elysia } from 'elysia';
 import { env } from './config/env';
 import { routes } from './routes/routes';
 
-const app = new Elysia().use(routes).listen(env.PORT ?? 8000);
-
-// eslint-disable-next-line no-console
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+new Elysia().use(routes).listen(env.PORT ?? 8000, ({ hostname, port }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🦊 Elysia is running at ${hostname}:${port}`);
+});
