929f8c7f6f37e2dfa61309e7a410be71294e491b
Joshua Santos <JoshCSan@gmail.com>
2023-09-17 19:47:37 -0700

    - fix: deployment hostname

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,14 @@ import { Elysia } from 'elysia';
 import { env } from './config/env';
 import { routes } from './routes/routes';
 
-new Elysia().use(routes).listen(env.PORT ?? 8000, ({ hostname, port }) => {
-  // eslint-disable-next-line no-console
-  console.log(`🦊 Elysia is running at ${hostname}:${port}`);
-});
+new Elysia().use(routes).listen(
+  {
+    hostname: '0.0.0.0',
+    port: env.PORT,
+  },
+  ({ hostname, port }) => {
+    // log the server information
+    // eslint-disable-next-line no-console
+    console.log(`🦊 Elysia is running at ${hostname}:${port}`);
+  },
+);
